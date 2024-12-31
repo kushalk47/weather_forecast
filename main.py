1d9aaca15e0a383d5bcb46a6b333ac39961dcd69
kushalk47 <115680607+kushalk47@users.noreply.github.com>
2024-12-31 14:41:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    filename=r"csv 2/TG_STAID"+str(station).zfill(6)+".txt"
+    filename=r"csv2/TG_STAID"+str(station).zfill(6)+".txt"
     df =pd.read_csv(filename,skiprows=20,parse_dates=['    DATE'])
     temperature=df.loc[df['    DATE']==date]['   TG'].squeeze()/10
     return {station: station, date: date, "temperature": temperature}
