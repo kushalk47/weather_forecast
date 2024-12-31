14cdf7fd0fbd50842b8f4a311cf3bf24343e1237
kushalk47 <kushalkantharaju536@gmail.com>
2024-12-31 14:18:50 +0530

    weather forecast api commit

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import pandas as pd
 
 app=Flask("__name__")
 
+stations =pd.read_csv(r"csv 2/stations.txt",skiprows=17)
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
     filename=r"csv 2/TG_STAID"+str(station).zfill(6)+".txt"
-    df =pd.read_csv(r"filename",skiprows=20,)
-    temperature =10
-    return {station:"station",date:"date",temperature:"23"}
+    df =pd.read_csv(filename,skiprows=20,parse_dates=['    DATE'])
+    temperature=df.loc[df['    DATE']==date]['   TG'].squeeze()/10
+    return {station: station, date: date, "temperature": temperature}
 
+@app.route("/api/v1/<station>")
+def stat(station):
+    filename = r"csv 2/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
 
 if __name__=="__main__":
     app.run(debug=True)
